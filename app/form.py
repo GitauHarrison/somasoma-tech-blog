bc303c54b30c149493b799d21acb4b5694dbca36
Gitau Harrison <harrisongitau3@gmail.com>
2020-04-30 05:42:25 +0300

    Fix the tab title error

--- app/form.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from wtforms.validators import DataRequired
-
-class Login(FlaskForm):
-    submit = SubmitField('Join Now')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, redirect, flash
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    return render_template('index.html', title='Home', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('You have requested for user {}, remember_me{} to join Somasoma'.format(form.username.data, form.remember_me.data))
+        redirect(url_for('index'))
+    return render_template('login.html', title='Join Now', form=form)
