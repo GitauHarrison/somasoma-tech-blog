2a33259c2ed0d423a32423e2a1eb4de821da03af
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-02 13:09:39 +0300

    Add a posts table in the db

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLACHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
