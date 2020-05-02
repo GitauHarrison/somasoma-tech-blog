22459d2277057b5727d9025ffe31a9aa6a8d18eb
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-02 14:20:10 +0300

    Add a posts table in the db

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from datetime import datetime
+
+# 1. Username
+# 2. Email
+# 3. Password
+# 4. id
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Posts', backref = 'author', lazy = 'dynamic')
+
+    def __repr__(self):
+        return 'User {}'.format(self.username)
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'Post {}'.format(self.body)
