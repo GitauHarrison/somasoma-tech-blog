198319f1e7420c14f77eff1cd3b301aefa4c15a9
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-05 05:08:57 +0300

    Create a registration form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Join Now')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired()])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register Now')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
