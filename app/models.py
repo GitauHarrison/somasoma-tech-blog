b316e91fb03ce2a4865b692f57a35e74d8c6cfa1
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-05 05:15:44 +0300

    Hash user passwords

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # 1. Username
 # 2. Email
@@ -15,6 +16,14 @@ class User(db.Model):
 
     def __repr__(self):
         return 'User {}'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key = True)
