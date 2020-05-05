05c3ba44f4cd7eab3e1edfb71e8579f27e8c6cca
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-05 05:21:18 +0300

    Get user by their id from the db

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 # 1. Username
 # 2. Email
 # 3. Password
 # 4. id
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
@@ -33,3 +35,7 @@ class Posts(db.Model):
 
     def __repr__(self):
         return 'Post {}'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
