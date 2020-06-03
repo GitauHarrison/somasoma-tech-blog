b80be09818a638b1261f3e1a55014c8d35472d42
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-03 03:34:59 +0300

    Fix bug of not naming function properly

--- app/models.py
+++ app/models.py
@@ -38,7 +38,7 @@ class User(UserMixin, db.Model):
             app.config['SECRET_KEY'], algorithm = 'HS256').decode('utf-8')
 
     @staticmethod
-    def verify_reset_password(token):
+    def verify_reset_password_token(token):
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'], algorithms = ['HS256'])['reset_password']
         except:
