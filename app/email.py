313c096ce5db9948a9ec693b167518f1f0b2f11e
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-03 05:35:34 +0300

    Fix bug

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from flask import render_template
 from threading import Thread
 
 def send_async_email(app, msg):
-    with app.context():
+    with app.app_context():
         mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
