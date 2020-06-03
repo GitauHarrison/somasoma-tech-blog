cae49cb20560723e4963b6f53a0d8a4b544c851d
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-03 03:59:10 +0300

    Add threading and multiprocessing when sending email to allow app to run faster

--- app/email.py
+++ app/email.py
@@ -2,6 +2,10 @@ from flask_mail import Message
 from app import mail, app
 from flask import render_template
 
+def send_async_email(app, msg):
+    with app.context():
+        mail.send(msg)
+
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
