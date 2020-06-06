b0ebaf3cdd46eef9b941d4df4d1fc7d2fb54391f
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-06 04:41:04 +0300

    Fix variable name bug

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Comments', backref = 'author', lazy = 'dynamic')
+    comments = db.relationship('Comments', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
         return 'User {}'.format(self.username)
