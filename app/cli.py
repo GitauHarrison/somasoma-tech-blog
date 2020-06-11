863103deabbf16fc65b425faf7966e026a550b34
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-11 15:42:58 +0300

    Add cli info for update command

--- app/cli.py
+++ app/cli.py
@@ -13,18 +13,19 @@ def init(lang):
     """Create language catalogue"""
     if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
         raise RuntimeError('extract command failed')
-    if os.system('pybabel init -i messages.pot -d app/translations' + lang):
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
         raise RuntimeError('init command failed')
     os.remove('messages.pot')
 
 @translate.command()
 def compile():
-    """Get language"""
+    """Compile all language"""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
 
 @translate.command()
 def update():
+    """Update all languages"""
     if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
         raise RuntimeError('extract command failed')
     if os.system('pybabel update -i messages.pot -d app/translations'):
