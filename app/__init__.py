5e601d61937017a206ec8691756d87a796d7fa2a
Gitau Harrison <harrisongitau3@gmail.com>
2020-06-11 14:38:43 +0300

    Force translation language from the app rather than change the browser setting to the new language

--- app/__init__.py
+++ app/__init__.py
@@ -58,4 +58,5 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'sw'
