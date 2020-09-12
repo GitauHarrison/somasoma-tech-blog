76d890985f2533f970cb8a5176acc2d4f411b792
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-12 04:56:16 +0300

    Fix indentation bug

--- app/__init__.py
+++ app/__init__.py
@@ -18,25 +18,23 @@ login.login_view = 'login'
 bootstrap = Bootstrap(app)
 mail = Mail(app)
 
-from app import routes, models, errors
-
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-        secure = ()
-    mail_handler = SMTPHandler(
-        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs=app.config['ADMINS'],
-        subject='Somasoma Failure',
-        credentials=auth, secure=secure
-    )
-    mail_handler.setLevel(logging.error)
-    app.logger.addHandler(mail_handler)
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Somasoma Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.error)
+        app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
@@ -49,3 +47,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Somasoma Blog')
+
+from app import routes, models, errors
