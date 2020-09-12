81a535305d10da5a6b68a3c4f52cc91579f5c5c0
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-12 07:04:50 +0300

    Add base file content implementing flask-bootstrap package

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, world!'
+    return render_template('home.html', title = 'Home')
