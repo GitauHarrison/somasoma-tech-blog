93ea245790af02dc5141199d3ae973c31689ed0f
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-12 07:36:04 +0300

    Display login form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title = 'Home')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for {}, remember me = {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', title = 'Log In', form = form)
