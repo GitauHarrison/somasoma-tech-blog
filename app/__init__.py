6396116af78e203deacd4fe4a562e96c6da1cb2e
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-13 05:57:16 +0300

    Send error message via email

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +13,23 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 bootstrap = Bootstrap(app)
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_SERVER'] and app.config['MAIL_PORT']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mailhandler = SMTPHandler(            
+            mailhost=app.config['MAIL_SERVER'],
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'][0],
+            subject='Somasoma: Error',
+            credentials=auth, secure=secure
+        )
+        mailhandler.setLevel(logging.ERROR)
+        app.logger.addHandler(mailhandler)
 
 from app import routes, models, errors
