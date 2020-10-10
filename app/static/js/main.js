23f8b9b3150e5d765fd07fc467b315846ffe0b8c
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-11 01:58:44 +0300

    Load payment page on another button click

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -21,4 +21,18 @@ fetch("/config")
       console.log(res);
     });
   });
+  // Event handler
+  document.querySelector("#submitBtn2").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create_checkout_session")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
 });
