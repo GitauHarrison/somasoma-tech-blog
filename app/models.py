a13f429633975981e08f7b750c44df02e4d393da
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-14 02:15:35 +0300

    Make changes for heroku deployment

--- app/models.py
+++ app/models.py
@@ -13,9 +13,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index =  True, unique = True)
     password_hash = db.Column(db.String(128))
 
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
-
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgres://bwtllxapoeoivu:e24fd0f855787958be8348aec5f6eb588cea7ce410e5a026c344cf78b1463b5a@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dc1q33b8qs0gmq'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER='smtp.gmail.com'
@@ -22,4 +23,4 @@ class Config(object):
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
-    DATABASE_URL = 'postgres://bwtllxapoeoivu:e24fd0f855787958be8348aec5f6eb588cea7ce410e5a026c344cf78b1463b5a@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dc1q33b8qs0gmq'
+    # DATABASE_URL = 'postgres://bwtllxapoeoivu:e24fd0f855787958be8348aec5f6eb588cea7ce410e5a026c344cf78b1463b5a@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dc1q33b8qs0gmq'
