cdfd51eedd7b9c3124e1df7eb92c0b6b4468e7d3
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-13 08:58:30 +0300

    Fix database tables issue

--- app/models.py
+++ app/models.py
@@ -19,8 +19,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index =  True, unique = True)
     password_hash = db.Column(db.String(128))
 
-    #about_me = db.Column(db.String(140))
-    #last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
@@ -89,7 +89,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return 'Post <>'.format(body    )
+        return 'Post <>'.format(self.body)
 
 @login.user_loader
 def load_user(id):
