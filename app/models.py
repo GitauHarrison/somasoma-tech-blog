b1feab49c54721d7845fa7f2bf39e6e98636187f
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-13 08:44:55 +0300

    Remove about_me and last_seen from models

--- app/models.py
+++ app/models.py
@@ -19,8 +19,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index =  True, unique = True)
     password_hash = db.Column(db.String(128))
 
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+    #about_me = db.Column(db.String(140))
+    #last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
