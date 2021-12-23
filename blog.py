261be66468e79ee7b622eb70829c617199c4c049
harry <harrisongitau3@gmail.com>
2021-12-23 16:57:26 +0300

    Dev: Add admin table in flask shell

--- blog.py
+++ blog.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import User, AnonymousTemplateInheritanceComment
+from app.models import User, AnonymousTemplateInheritanceComment, Admin
 
 
 @app.shell_context_processor
@@ -7,5 +7,6 @@ def make_shell_context():
     return dict(
         db=db,
         User=User,
+        Admin=Admin,
         AnonymousTemplateInheritanceComment=AnonymousTemplateInheritanceComment
         )
