bcde1498411d5b85827ef13f64e1e258456e9900
harry <harrisongitau3@gmail.com>
2021-12-23 16:33:52 +0300

    Dev: Add form configuration

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config(object):
         'postgres://', 'postgresql://') or\
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Comments
+    POSTS_PER_PAGE = int(os.environ.get('POSTS_PER_PAGE') or 10)
+
+    # Form
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
