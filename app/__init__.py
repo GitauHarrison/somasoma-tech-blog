52489f33bc3338ce739fe029ecdda40ff2d803b4
harry <harrisongitau3@gmail.com>
2021-12-23 12:36:11 +0300

    Dev: Create dependancies objects

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
+mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors
