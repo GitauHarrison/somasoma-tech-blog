c5b1ce822d1f88fca497bf4da890128c2ca71e7b
harry <harrisongitau3@gmail.com>
2021-12-23 16:00:06 +0300

    Dev: Update path to error templates

--- app/errors.py
+++ app/errors.py
@@ -5,7 +5,7 @@ from flask import render_template
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template(
-        '404.html',
+        'errors/404.html',
         title='Page Not Found'
         ), 404
 
@@ -14,6 +14,6 @@ def page_not_found(error):
 def internal_error(error):
     db.session.rollback()
     return render_template(
-        '500.html',
+        'errors/500.html',
         title='Internal Error'
         ), 500
