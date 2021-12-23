8578caf18c5d422edcc707dd5e07ae494ee163c0
harry <harrisongitau3@gmail.com>
2021-12-23 20:53:58 +0300

    Dev: Add forms to update the blog

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, TextAreaField, PasswordField, Bool
 from wtforms.validators import DataRequired, Length, Email, EqualTo,\
     ValidationError
 from app.models import Admin
+from flask_wtf.file import FileField
 
 
 # Anonymous Comment form
@@ -72,3 +73,89 @@ class RegisterForm(FlaskForm):
         admin = Admin.query.filter_by(email=email.data).first()
         if admin is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class UpdateBlogForm(FlaskForm):
+    img = FileField('Update Blog Image')
+    title = StringField(
+        'Title',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    body = TextAreaField(
+        'Body',
+        validators=[DataRequired()]
+        )
+    link = StringField(
+        'Link',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    submit = SubmitField('Update')
+
+
+class UpdateEventsForm(FlaskForm):
+    img = FileField('Update Events Image')
+    title = StringField(
+        'Title',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    event_date = StringField(
+        'Event Date',
+        validators=[DataRequired(), Length(min=2, max=20)],
+        render_kw={'placeholder': 'Thursday, December 23, 2021'}
+        )
+    event_time = StringField(
+        'Event Time',
+        validators=[DataRequired(), Length(min=2, max=20)],
+        render_kw={'placeholder': '6.00 PM - 8.00 PM EAT'}
+        )
+    location = StringField(
+        'Location',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    body = TextAreaField(
+        'Body',
+        validators=[DataRequired()]
+        )
+    meet_link = StringField(
+        'Meet Link',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    submit = SubmitField('Update')
+
+
+class UpdateCoursesForm(FlaskForm):
+    img = FileField('Update Courses Image')
+    title = StringField(
+        'Title',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    body = TextAreaField(
+        'Body',
+        validators=[DataRequired()]
+        )
+    overview = TextAreaField(
+        'Overview',
+        validators=[DataRequired()]
+    )
+    next_class_date = StringField(
+        'Next Class Date',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    link = StringField(
+        'Link',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    submit = SubmitField('Update')
+
+
+class StudentStoriesForm(FlaskForm):
+    img = FileField('Update Student Stories Image')
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    body = TextAreaField(
+        'Body',
+        validators=[DataRequired()]
+        )
+    submit = SubmitField('Update')
