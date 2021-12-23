5d40d2d66b03d11e4938dc3852b46524fbdd1ce3
harry <harrisongitau3@gmail.com>
2021-12-23 17:15:10 +0300

    Dev: Create login and registration forms

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,73 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo,\
+    ValidationError
+from app.models import Admin
 
 
 # Anonymous Comment form
 
 
 class AnonymousCommentForm(FlaskForm):
-    name = StringField('Name',
-                       validators=[DataRequired(), Length(min=2, max=20)]
-                       )
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()]
-                        )
-    comment = TextAreaField('Comment',
-                            validators=[DataRequired()]
-                            )
+    name = StringField(
+        'Name',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+        )
+    comment = TextAreaField(
+        'Comment',
+        validators=[DataRequired()]
+        )
     submit = SubmitField('Post')
+
+
+# Admin
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    password = StringField(
+        'Password',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+        )
+    password = StringField(
+        'Password',
+        validators=[DataRequired(), Length(min=2, max=20)]
+        )
+    confirm_password = StringField(
+        'Confirm Password',
+        validators=[
+            DataRequired(),
+            Length(min=2, max=20),
+            EqualTo('password')
+            ]
+        )
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        admin = Admin.query.filter_by(username=username.data).first()
+        if admin is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        admin = Admin.query.filter_by(email=email.data).first()
+        if admin is not None:
+            raise ValidationError('Please use a different email address.')
