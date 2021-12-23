d51ffe00812459c0751662ebff808c270d22439d
harry <harrisongitau3@gmail.com>
2021-12-23 16:17:59 +0300

    Dev: Create a user model

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from app import db
+from hashlib import md5
+
+# Anonymous User
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True)
+    comment = db.Column(db.String(140))
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
