073a88caaa6505718d4b556c494dea9b96097a22
harry <harrisongitau3@gmail.com>
2021-12-23 16:21:58 +0300

    Dev: Add anonymous template inheritance table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from hashlib import md5
+from datetime import datetime
 
 # Anonymous User
 
@@ -11,6 +12,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True)
     comment = db.Column(db.String(140))
 
+    comments = db.relationship(
+        'AnonymousTemplateInheritanceComment',
+        backref='author',
+        lazy='dynamic'
+        )
+
     def __repr__(self):
         return f'User {self.name}'
 
@@ -18,3 +25,22 @@ class User(db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+# ============================================================
+# ANONYMOUS COURSE CONTENT
+# ============================================================
+
+
+class AnonymousTemplateInheritanceComment(db.Model):
+    __tablename__ = 'template inheritance comment'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Template Inheritance Comment: {self.body}'
+
+# ============================================================
+# ANONYMOUS COURSE CONTENT
+# ============================================================
