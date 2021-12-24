b29f544469ef56a8c1b0210c1662b37b10ebf253
harry <harrisongitau3@gmail.com>
2021-12-24 23:33:24 +0300

    Fix: Add email configurations

--- config.py
+++ config.py
@@ -26,3 +26,11 @@ class Config(object):
 
     # Heroku logs
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+    # Email Support
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.environ.get('ADMINS')
