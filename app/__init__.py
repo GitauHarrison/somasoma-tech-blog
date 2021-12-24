e2842c4249e04839ae9968ad90b30e98f4536654
harry <harrisongitau3@gmail.com>
2021-12-24 23:08:17 +0300

    Dev: Log error messages

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_login import LoginManager
 from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -47,6 +51,44 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Somasoma Tech Blog Failure',
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler(
+                'logs/somasoma.log',
+                maxBytes=10240,
+                backupCount=10
+                )
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('somasoma tech blog')
 
     return app
 
